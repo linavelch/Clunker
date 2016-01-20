05a4d0f55153c44cdc6b9385880ddd63130e753e
Lina Velcheva <linavelcheva@Linas-MacBook-Pro.local>
2016-01-20 17:38:49 -0500

    First Save

---
+++ clunker_app.js
@@ -0,0 +1,33 @@
+if (Meteor.isClient) {
+  Template.signup.events({
+    'submit form': function(event) {
+      event.preventDefault();
+      var emailVar = event.target.signupEmail.value;
+      var passwordVar = event.target.signupPassword.value;
+      Accounts.createUser({
+        email: emailVar,
+        password: passwordVar
+      });
+    }
+  });
+  Template.login.events({
+    'submit form': function(event) {
+      event.preventDefault();
+      var emailVar = event.target.loginEmail.value;
+      var passwordVar = event.target.loginPassword.value;
+      Meteor.loginWithPassword(emailVar, passwordVar);
+    }
+  });
+  Template.settings.events({
+    'click .logout': function(event) {
+      event.preventDefault();
+      Meteor.logout();
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
