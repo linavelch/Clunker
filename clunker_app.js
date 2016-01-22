8720d84c6226dc23de0163013aca7965e93fb71c
Lina Velcheva <linavelcheva@Linas-MacBook-Pro.local>
2016-01-22 17:54:40 -0500

    Routing Attempt for Listings

--- clunker_app.js
+++ clunker_app.js
@@ -1,3 +1,12 @@
+Router.route('/listings', function() {
+	this.render('listings');
+	this.layout('layout');
+   {
+  name: 'listings.show'
+}
+});
+
+
 if (Meteor.isClient) {
   Template.signup.events({
     'submit form': function(event) {
@@ -16,7 +25,10 @@ if (Meteor.isClient) {
       var emailVar = event.target.loginEmail.value;
       var passwordVar = event.target.loginPassword.value;
       Meteor.loginWithPassword(emailVar, passwordVar);
-    }
+    if (false) {
+      confirm('Wrong email or password!');
+     }
+  }
   });
   Template.settings.events({
     'click .logout': function(event) {
@@ -34,4 +46,14 @@ if (Meteor.isServer) {
     restrictCreationByEmailDomain: 'williams.edu',
     sendVerificationEmail: true
   });
+  Accounts.emailTemplates.verifyEmail.subject = function (user) {
+    return "Please Verify Your ClunkerU Account";
+  };
+  Accounts.emailTemplates.verifyEmail.text = function(user, url) {
+    var text = "Hello,\n\nThank you for registering to ClunkerU.\n\nTo complete your registration" +
+    "and verify your ClunkerU account, please use the following link.\n\n" + url + "\n\n" +
+               "Thank you,\nThe ClunkerU Team";
+    return text;
+  };
+  Accounts.emailTemplates.from = "ClunkerU Accounts <no-reply@meteor.com>"
 }
